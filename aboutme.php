e0cfa380e6821a360512a579f346995bac34c377
Dominic Cuneo <cuneo94@gmail.com>
2016-01-13 13:40:44 -0700

    added php file

---
+++ aboutme.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>About Me</title>
+	</head>
+	<body>
+		<p> My name Dominic Cuneo I am 21 years old. I am enrolled at CNM DeepDive coding bootcamp</p>
+		<p>I was  born in Nashville Tennesse  </p>
+		<img src="http://media1.rent.com/oneweb/img/local-info/nashville-tn-cityscape.jpg">
+		<p>I move to Arab Alabama in 8th grade</p>
+
+	</body>
+</html>
