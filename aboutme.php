44f43b430f6faad36a733a61e310cb13574f0ebb
Dominic Cuneo <cuneo94@gmail.com>
2016-01-13 14:03:12 -0700

    added php file

--- aboutme.php
+++ aboutme.php
@@ -9,6 +9,13 @@
 		<p>I was  born in Nashville Tennesse  </p>
 		<img src="http://media1.rent.com/oneweb/img/local-info/nashville-tn-cityscape.jpg">
 		<p>I move to Arab Alabama in 8th grade</p>
-
+		<h3>Here are some things poeple loved to do in Bama.</h3>
+		<table>
+			<tbody>
+				<tr>
+				<td>Watch some college football ROLL TIDE!!</td>
+				</tr>
+			</tbody>
+		</table>
 	</body>
 </html>
