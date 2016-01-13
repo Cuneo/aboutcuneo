d9378aeaa6096865c45f35db14783ab8872efcc4
Dominic Cuneo <cuneo94@gmail.com>
2016-01-13 16:44:06 -0700

    added css and a list

--- aboutme.php
+++ aboutme.php
@@ -2,6 +2,7 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
+		<link type="text/css" rel="stylesheet" href="css/stylesheet.css" /><!--the href="css/'this must  be the same as the name of the css file'.css"-->
 		<title>About Me</title>
 	</head>
 	<body>
@@ -9,13 +10,13 @@
 		<p>I was  born in Nashville Tennesse  </p>
 		<img src="http://media1.rent.com/oneweb/img/local-info/nashville-tn-cityscape.jpg">
 		<p>I move to Arab Alabama in 8th grade</p>
-		<h3>Here are some things poeple loved to do in Bama.</h3>
-		<table>
-			<tbody>
-				<tr>
-				<td>Watch some college football ROLL TIDE!!</td>
-				</tr>
-			</tbody>
-		</table>
+		<h1>Here are some things people loved to do in Bama.</h1>
+		<ul>
+			<li>Watch some awesome college football <strong>ROLL TIDE</strong>!!<br>
+				<img src="http://2f13yq12csmv2yraq925m73i.wpengine.netdna-cdn.com/wp-content/uploads/2010/06/AlabamaPage-650x342.jpg"></li>
+			<li>Getting covered Mudding<a href="https://www.youtube.com/watch?v=JWViIC_iV-o"><img src="http://www.world-of-atvs.com/images/atv-mudding-01.jpg"></a></li>
+			<li>Hunting tasty animals </li>
+		</ul>
+
 	</body>
 </html>
